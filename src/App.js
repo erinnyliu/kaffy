efefc822ddcbd7ff4c7b6e04724cfd82e2a606b2
Erin Liu <erin.y.liu@gmail.com>
2020-07-11 20:12:41 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import title from './../src/img/title.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">Hello</div>
+      {/* test */}
+      <img src={title} alt="Logo" height = "60%" width= "100%" />
     </div>
   );
 }
